2a1b00b3c2d97575b9418dd81d73404543c512d6
Isaev Denis <4648736+jirfag@users.noreply.github.com>
2018-06-07 08:37:31 +0300

    Update manually to v1.5

--- Formula/golangci-lint.rb
+++ Formula/golangci-lint.rb
@@ -1,9 +1,9 @@
 class GolangciLint < Formula
   desc "Fast linters runner for Go."
   homepage "https://golangci.com"
-  url "https://github.com/golangci/golangci-lint/releases/download/v1.4.1/golangci-lint-1.4.1-darwin-amd64.tar.gz"
-  version "1.4.1"
-  sha256 "0592e8869ddf01d2f9c6d332808b118e0fb6aee9a5d786b71183ebc341fe584a"
+  url "https://github.com/golangci/golangci-lint/releases/download/v1.5/golangci-lint-1.5-darwin-amd64.tar.gz"
+  version "1.5"
+  sha256 "d7f85a9da511b4c2242bb0b558ddcb6c66335224f7e0212b3c8127e20bf81de6"
 
   def install
     bin.install "golangci-lint"
