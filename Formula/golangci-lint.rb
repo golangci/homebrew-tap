d2df2e2512faa6bb18695e04587b2b292b0d6935
goreleaserbot <goreleaser@carlosbecker.com>
2018-06-18 09:56:51 +0300

    Brew formula update for golangci-lint version v1.7

--- Formula/golangci-lint.rb
+++ Formula/golangci-lint.rb
@@ -1,9 +1,9 @@
 class GolangciLint < Formula
   desc "Fast linters runner for Go."
   homepage "https://golangci.com"
-  url "https://github.com/golangci/golangci-lint/releases/download/v1.6.1/golangci-lint-1.6.1-darwin-amd64.tar.gz"
-  version "1.6.1"
-  sha256 "1fdf5250d4c0f4aac7062b1efe21783094795ddd22fb5e7d574653e1ebb9ec02"
+  url "https://github.com/golangci/golangci-lint/releases/download/v1.7/golangci-lint-1.7-darwin-amd64.tar.gz"
+  version "1.7"
+  sha256 "01408fe5cbef6fa98d96fbb32045c895ae8a2bf2376fd8cf6eb10c624ede13d6"
 
   def install
     bin.install "golangci-lint"
