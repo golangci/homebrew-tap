b26f50a392bca8b1867c83dc7dfadc132cf2c392
goreleaserbot <goreleaser@carlosbecker.com>
2020-01-20 11:07:48 +0300

    Brew formula update for golangci-lint version v1.23.1

--- Formula/golangci-lint.rb
+++ Formula/golangci-lint.rb
@@ -2,24 +2,24 @@
 class GolangciLint < Formula
   desc "Fast linters runner for Go."
   homepage "https://golangci.com"
-  version "1.23.0"
+  version "1.23.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/golangci/golangci-lint/releases/download/v1.23.0/golangci-lint-1.23.0-darwin-amd64.tar.gz"
-    sha256 "7b3d2acdda2790955e0b17f065ade01fea98c34e036600465ff855b7738bd235"
+    url "https://github.com/golangci/golangci-lint/releases/download/v1.23.1/golangci-lint-1.23.1-darwin-amd64.tar.gz"
+    sha256 "f54049ce414fcb38c321be0420216d3043849f102ea9cd1752fab98262924efa"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/golangci/golangci-lint/releases/download/v1.23.0/golangci-lint-1.23.0-linux-amd64.tar.gz"
-      sha256 "38fc818129b8f0d17cb634ff73f3eb071f3a0ea68b5b433345afcbbd2ef47508"
+      url "https://github.com/golangci/golangci-lint/releases/download/v1.23.1/golangci-lint-1.23.1-linux-amd64.tar.gz"
+      sha256 "fb4ad63d56072219ef2ace58ba2de061ec705055f80c986240a6e403a1f64776"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/golangci/golangci-lint/releases/download/v1.23.0/golangci-lint-1.23.0-linux-arm64.tar.gz"
-        sha256 "40ab54cdffa3bbf02ac28e8263dcfae602407eca2fb9730c2479a11b94278dbf"
+        url "https://github.com/golangci/golangci-lint/releases/download/v1.23.1/golangci-lint-1.23.1-linux-arm64.tar.gz"
+        sha256 "e15d4122965414adb81ef23dd03a5730df4e32bc7a1d1c2a620377ca9fd34357"
       else
-        url "https://github.com/golangci/golangci-lint/releases/download/v1.23.0/golangci-lint-1.23.0-linux-armv6.tar.gz"
-        sha256 "fccea0af13a4fb970b817d04c927cddd9f3b2b9cb3663727b1166db40c3c5623"
+        url "https://github.com/golangci/golangci-lint/releases/download/v1.23.1/golangci-lint-1.23.1-linux-armv6.tar.gz"
+        sha256 "1d18b9cd6c5ba134f3276c3daebef645a6f4e11c26a0f3c14ad505a44548bc2a"
       end
     end
   end
