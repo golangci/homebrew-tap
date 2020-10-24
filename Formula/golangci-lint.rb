708a8ca15035740514df7cda6266c8632baa4de0
goreleaserbot <goreleaser@carlosbecker.com>
2020-10-25 02:43:19 +0300

    Brew formula update for golangci-lint version v1.32.0

--- Formula/golangci-lint.rb
+++ Formula/golangci-lint.rb
@@ -2,24 +2,24 @@
 class GolangciLint < Formula
   desc "Fast linters runner for Go."
   homepage "https://golangci.com"
-  version "1.31.0"
+  version "1.32.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/golangci/golangci-lint/releases/download/v1.31.0/golangci-lint-1.31.0-darwin-amd64.tar.gz"
-    sha256 "f3afeb6ad6964615e2b238f56cc2e5b32464f2f302a4f3ccf5874a04170c287a"
+    url "https://github.com/golangci/golangci-lint/releases/download/v1.32.0/golangci-lint-1.32.0-darwin-amd64.tar.gz"
+    sha256 "294bca5902a5c992345dc773549cabcf25029383aa3bafd06cf65d0164b22faf"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/golangci/golangci-lint/releases/download/v1.31.0/golangci-lint-1.31.0-linux-amd64.tar.gz"
-      sha256 "9a5d47b51442d68b718af4c7350f4406cdc087e2236a5b9ae52f37aebede6cb3"
+      url "https://github.com/golangci/golangci-lint/releases/download/v1.32.0/golangci-lint-1.32.0-linux-amd64.tar.gz"
+      sha256 "774fc71efc2b41327aeca7bdff335c6387ff3750e0cfd5cc18a64709ba1e412a"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/golangci/golangci-lint/releases/download/v1.31.0/golangci-lint-1.31.0-linux-arm64.tar.gz"
-        sha256 "9fff85f4649d17d18ebbcb775fec05de42a83e08787af1850f8f5f8dd4c066e9"
+        url "https://github.com/golangci/golangci-lint/releases/download/v1.32.0/golangci-lint-1.32.0-linux-arm64.tar.gz"
+        sha256 "1571229e6a74f17e83849d2241f214222e236701258e551def306495364d7490"
       else
-        url "https://github.com/golangci/golangci-lint/releases/download/v1.31.0/golangci-lint-1.31.0-linux-armv6.tar.gz"
-        sha256 "d9ce8edd34994abead106f6a58337ca9396c8d0b1352bb19f7cd731ea9ea2021"
+        url "https://github.com/golangci/golangci-lint/releases/download/v1.32.0/golangci-lint-1.32.0-linux-armv6.tar.gz"
+        sha256 "b6c0ef8ad1136054768b30073e6c8d4ecbd185fad9608d57ac83f4107b37904d"
       end
     end
   end
@@ -30,6 +30,8 @@ class GolangciLint < Formula
     (bash_completion/"golangci-lint").write output
     output = Utils.popen_read("#{bin}/golangci-lint completion zsh")
     (zsh_completion/"_golangci-lint").write output
+    output = Utils.popen_read("#{bin}/golangci-lint completion fish")
+    (fish_completion/"eksctl.fish").write output
     prefix.install_metafiles
   end
 
