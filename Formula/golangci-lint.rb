6715835097af5798e8772ae3ee97cb20c747f69c
goreleaserbot <goreleaser@carlosbecker.com>
2022-02-17 14:45:50 +0300

    Brew formula update for golangci-lint version v1.44.1

--- Formula/golangci-lint.rb
+++ Formula/golangci-lint.rb
@@ -5,12 +5,12 @@
 class GolangciLint < Formula
   desc "Fast linters runner for Go."
   homepage "https://golangci.com"
-  version "1.44.0"
+  version "1.44.1"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/golangci/golangci-lint/releases/download/v1.44.0/golangci-lint-1.44.0-darwin-amd64.tar.gz"
-      sha256 "69dedd13f203acec13d9952994c7c2bed8530fb1c85d75d923454624f000771c"
+      url "https://github.com/golangci/golangci-lint/releases/download/v1.44.1/golangci-lint-1.44.1-darwin-amd64.tar.gz"
+      sha256 "b82a3d2973623da79eef22aa758e0fa6c923bf5baafed9db99dbbd33de3e2de4"
 
       def install
         bin.install "golangci-lint"
@@ -24,8 +24,8 @@ class GolangciLint < Formula
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/golangci/golangci-lint/releases/download/v1.44.0/golangci-lint-1.44.0-darwin-arm64.tar.gz"
-      sha256 "1d05c24d3be9cf7c73ab625a16e0ec237a8289ef4078d5cd52bedaaab41a0ea7"
+      url "https://github.com/golangci/golangci-lint/releases/download/v1.44.1/golangci-lint-1.44.1-darwin-arm64.tar.gz"
+      sha256 "18ef728b0996f05603c32345e669cdab23bfef7845d21469cb6a03df812d9eeb"
 
       def install
         bin.install "golangci-lint"
@@ -42,23 +42,8 @@ class GolangciLint < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/golangci/golangci-lint/releases/download/v1.44.0/golangci-lint-1.44.0-linux-amd64.tar.gz"
-      sha256 "e059c301ca0bcc0af114586a7f54bab897a5a7830b81a75f614933d9e018e1a4"
-
-      def install
-        bin.install "golangci-lint"
-        output = Utils.popen_read("#{bin}/golangci-lint completion bash")
-        (bash_completion/"golangci-lint").write output
-        output = Utils.popen_read("#{bin}/golangci-lint completion zsh")
-        (zsh_completion/"_golangci-lint").write output
-        output = Utils.popen_read("#{bin}/golangci-lint completion fish")
-        (fish_completion/"eksctl.fish").write output
-        prefix.install_metafiles
-      end
-    end
-    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/golangci/golangci-lint/releases/download/v1.44.0/golangci-lint-1.44.0-linux-armv6.tar.gz"
-      sha256 "a981a9b95a048979eb06fd329588b157c57731d7822ee44bb4f4489863dbaa0d"
+      url "https://github.com/golangci/golangci-lint/releases/download/v1.44.1/golangci-lint-1.44.1-linux-amd64.tar.gz"
+      sha256 "e7f2dc0e8a1eb5652c3570fd371d69899aa7efa19eb3db3dbc6b7948ce4a7ce9"
 
       def install
         bin.install "golangci-lint"
@@ -72,8 +57,23 @@ class GolangciLint < Formula
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/golangci/golangci-lint/releases/download/v1.44.0/golangci-lint-1.44.0-linux-arm64.tar.gz"
-      sha256 "f72abcb03b0baf31dcae8c1fee5ab3a17b12d46c6281b1b959e3dd1a11f5655f"
+      url "https://github.com/golangci/golangci-lint/releases/download/v1.44.1/golangci-lint-1.44.1-linux-arm64.tar.gz"
+      sha256 "b8680dd514449d76e224e78e5f746fca75c37e6764d3c5cb07590a15187e03d1"
+
+      def install
+        bin.install "golangci-lint"
+        output = Utils.popen_read("#{bin}/golangci-lint completion bash")
+        (bash_completion/"golangci-lint").write output
+        output = Utils.popen_read("#{bin}/golangci-lint completion zsh")
+        (zsh_completion/"_golangci-lint").write output
+        output = Utils.popen_read("#{bin}/golangci-lint completion fish")
+        (fish_completion/"eksctl.fish").write output
+        prefix.install_metafiles
+      end
+    end
+    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+      url "https://github.com/golangci/golangci-lint/releases/download/v1.44.1/golangci-lint-1.44.1-linux-armv6.tar.gz"
+      sha256 "4b54beb9e8b03124fde339d6de88b8684575a467af8950077f0afefcaadcbcf3"
 
       def install
         bin.install "golangci-lint"
